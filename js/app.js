c15675afcd440d7a98a17ebae985d464e2147996
Prodigy - <peeddurple@gmail.com>
2019-02-13 02:10:59 -0400

    js/app.js hadling cache storage and offline mode

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw-lifecycle.js')
-        .then(reg => {
-            setTimeout(() => {
-                reg.sync.register('posteo-gatitos');
-            }, 3000);
-        });
+    navigator.serviceWorker.register('/sw.js');
 
 }
 
-/*fetch('https://reqres.in/api/users')
-    .then(resp => resp.text())
-    .then(console.log);*/
+if (window.caches) {
+    caches.open('cache-v1.0')
+        .then(cache => {
+
+            cache.addAll([
+                '/index.html',
+                '/css/style.css',
+                '/img/main.jpg'
+            ]).then(() => {
+
+                //testing cache deleting file
+                cache.delete('/css/style.css');
+
+
+                //here as we see we put any raw text in the specified file, so
+                //we can update here that file with the last version in the server
+                //querying it to the server
+                cache.put('index.html', new Response('hola mundo'))
+            });
+
+            /*cache.match('/index.html')
+                .then(res => {
+
+                });*/
+
+            //printing the key names of the cache files at the cache storage
+            caches.keys().then(console.log);
+
+        });
+}
---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('fetch', event => {
+
+    const offlineHTML = fetch('pages/offline.html')
+        .then(resp => resp.text());
+
+    const offlineResp = new Response('pages/offline.html', {
+        headers: {
+            'Content': 'text/html'
+        }
+    });
+
+    const resp = fetch(event.request)
+        .catch(() => {
+            return offlineResp;
+        });
+
+
+
+    event.respondWith(resp);
+
+});
